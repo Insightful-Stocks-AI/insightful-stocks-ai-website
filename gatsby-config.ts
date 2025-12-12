39b9b0c78ea6ca4aee273e61cdc49d3ebac5a804
Justin Cianci <justinc328@gmail.com>
2025-12-11 19:35:51 -0500

    google analytics gtag

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,38 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    // Only enable Google Analytics in production (not on localhost)
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+              // Google Analytics 4 Measurement ID
+              trackingIds: [
+                "G-E17M5MMYCT",
+              ],
+              // This object gets passed directly to the gtag config command
+              // This config will be shared across all trackingIds
+              gtagConfig: {
+                anonymize_ip: true,
+                cookie_expires: 0,
+              },
+              // This object is used for configuration specific to this plugin
+              pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: true,
+                // Does not respect Do Not Track
+                respectDNT: false,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+              },
+            },
+          },
+        ]
+      : []),
+  ]
 };
 
 export default config;
